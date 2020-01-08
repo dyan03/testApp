8b4931c83c19f66f6adda3dd6a27d7b28924f23d
DESKTOP-AT1HIJB\ahn <theta93@gmail.com>
2020-01-08 23:17:11 +0900

    set router-middleware

--- serverExpress.js
+++ serverExpress.js
@@ -1,40 +1,13 @@
 var express= require("express")
 
-app=express()
-
+var app=express()
+var router=require("./router/main")(app)
 var port= process.env.port||8080
 
 app.set('views',__dirname+"/views")
 app.set('view engine', 'ejs')
 
-app.use("/responseUse",function(req,res){
- 
-    console.log("use console response")
-    res.json("use request completed")
-})
-
-app.get("/main",function(req, res){
-    
-    res.render()
-})
-
-var cb0 = function (req, res, next) {
-    console.log('CB0');
-    next();
-}
-
-var cb1 = function (req, res, next) {
-    console.log('CB1');
-    next();
-}
-
-app.get('/example/a', [cb0, cb1], function (req, res, next) {
-    console.log('the response will be sent by the next function ...');
-    next();
-}, function (req, res) {
-    res.send('Hello from A!');
-});
-
+app.engine('html', require('ejs').renderFile);
 
 app.listen(port)
 
