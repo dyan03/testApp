0c359065eb85f5e7fb27c3e1c260ea32160b2dea
DESKTOP-AT1HIJB\ahn <theta93@gmail.com>
2020-01-08 22:30:21 +0900

    cb

--- serverExpress.js
+++ serverExpress.js
@@ -18,6 +18,24 @@ app.get("/main",function(req, res){
     res.render()
 })
 
+var cb0 = function (req, res, next) {
+    console.log('CB0');
+    next();
+}
+
+var cb1 = function (req, res, next) {
+    console.log('CB1');
+    next();
+}
+
+app.get('/example/a', [cb0, cb1], function (req, res, next) {
+    console.log('the response will be sent by the next function ...');
+    next();
+}, function (req, res) {
+    res.send('Hello from A!');
+});
+
+
 app.listen(port)
 
 console.log("Listening on port", port)
